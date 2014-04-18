f4b3848306fc4e67e81408a4347a6a50ef1e6c66
Mike Fourie <michaelfourie@gmail.com>
2014-04-18 21:11:44 +0100

    global exception handler

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,34 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="App.xaml.cs" company="FreeToDev"> (c) Mike Fourie. All other rights reserved.</copyright>
 // --------------------------------------------------------------------------------------------------------------------
+
+using System.Globalization;
+
 namespace LineCounter
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            this.Dispatcher.UnhandledException += this.OnDispatcherUnhandledException;
+        }
+
+        internal void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exception = e.Exception.ToString();
+            if (e.Exception.InnerException != null)
+            {
+                exception += ". Inner Exception = " + e.Exception.InnerException;
+            }
+
+            string errorMessage = string.Format(CultureInfo.CurrentCulture, "{0}. Please report this error", exception);
+            MessageBox.Show(errorMessage, "Sorry, this was not meant to happen.", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
